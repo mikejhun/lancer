ba332c3cde5bad6fb242f3befcb3775c2ef6fb9b
Michael Jhun <mikejhun@gmail.com>
2015-12-02 01:24:18 -0500

    Create Gig, Show Page, Footers

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,10 +1,25 @@
 class GigsController < ApplicationController
 
 	def index
-
 	end
 
 	def new
+		@gig = Gig.new
+	end
+
+	def create
+		@gig = Gig.new(gig_params)
+		@gig.save
+		redirect_to @gig
+	end
+
+	def show
+		@gig = Gig.find(params[:id])
+	end
+
+	private
+		def gig_params
+			params.require(:gig).permit(:name, :description, :budget, :location, :open)
+		end
 
 	end
-end
